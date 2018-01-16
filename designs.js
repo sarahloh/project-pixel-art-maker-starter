2676d0f0b6ba54b06ca780495665bbc1a219b6aa
Sarah Loh <sarah.loh0708@gmail.com>
2018-01-16 12:07:43 +0000

    Created grid, added color change functionality

--- designs.js
+++ designs.js
@@ -1,10 +1,42 @@
-// Select color input
-// Select size input
+$(document).ready(function () {
 
-// When size is submitted by the user, call makeGrid()
+/**
+ * @description Creates a new canvas
+ * @param {number} width
+ * @param {number} height
+ */
+function makeGrid(rows, cols) {
+    console.log(`rows: ${rows}, columns: ${cols}`);
 
-function makeGrid() {
+    const canvas = $('#pixel_canvas');
 
-// Your code goes here!
+    // clear previous canvas
+    canvas.empty();
 
+    // add rows and columns to table
+    for (let row = 0; row < rows; row++) {
+        canvas.append("<tr></tr>");
+    }
+    for (let col = 0; col < cols; col++) {
+        $('tr').append("<td></td>");
+    }
 }
+
+// Event listener - grid size submitted
+$('#size_picker').submit(function(e) {
+    // prevent form submit from reloading page
+    e.preventDefault();
+
+    const height = $('#input_height').val();
+    const width = $('#input_width').val();
+
+    makeGrid(height, width);
+});
+
+// Event listener - canvas clicked
+$('#pixel_canvas').on('click', 'td', function(e) {
+    const cell = $(e.target);
+    cell.css('background-color', $('#color_picker').val());
+});
+
+});
