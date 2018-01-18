ee7c89d4dda959ba83111f07680f9b1b92523edb
Sarah Loh <sarah.loh0708@gmail.com>
2018-01-18 20:09:42 +0000

    Working on design

--- designs.js
+++ designs.js
@@ -1,30 +1,37 @@
-$(document).ready(function () {
-
+// Global variables
 const canvas = $('#pixel_canvas');
 const gridForm = $('#size_picker');
+// Used to detect mouse drag for painting effect
+let isDown = false;
 
 /**
- * @description Creates a new canvas
+ * @description Creates a new grid
  * @param {number} height
- * @param {number} wodth
+ * @param {number} width
  */
 function makeGrid(height, width) {
 
+    // Check if first canvas created, otherwise clear existing one
     if (canvas.is(':empty')) {
-        // change button to 'Reset' after first grid created
-        console.log('First canvas');
-        $('input[type=submit]').val("Reset");
+        // Change button to 'Reset' after first grid created
+        let gridButton = $('input[type=submit]');
+        gridButton.val("Reset");
+
+        // Show instructions paragraphs
+        $('.instructions').slideDown(600);
     } else {
-        // clear canvas
+        // Clear canvas
         canvas.empty();
     }
 
-    // add rows and columns to table
+    // Build the grid
     for (let row = 0; row < height; row++) {
-        canvas.append("<tr></tr>");
+        canvas.append('<tr></tr>');
     }
     for (let col = 0; col < width; col++) {
-        $('tr').append("<td></td>");
+        setTimeout(function () {
+            $('tr').append('<td></td>');
+        }, col * 50);
     }
 }
 
@@ -33,17 +40,28 @@ gridForm.submit(function(e) {
     let height = $('#input_height').val();
     let width = $('#input_width').val();
 
-    // prevent form submit from reloading page
+    // Prevent form submit from reloading page
     e.preventDefault();
 
+    // Call function to create a grid
     makeGrid(height, width);
 });
 
-// Event listener - canvas clicked
-canvas.on('click', 'td', function(e) {
-    let cell = $(e.target);
-    let currentColor = $('#color_picker').val();
-    cell.css('background-color', currentColor);
-});
+// Store if mouse is up or down - used to detect mouse drag
+$(document)
+    .mousedown(function () {
+        isDown = true;
+    })
+    .mouseup(function () {
+        isDown = false;
+    }
+);
 
+// Event listener - canvas clicked or moved with mouse down
+canvas.on('click mousemove', 'td', function (e) {
+    if (e.type == "click" || isDown) {
+        let cell = $(e.target);
+        let currentColor = $('#color_picker').val();
+        cell.css('background-color', currentColor);
+    }
 });
